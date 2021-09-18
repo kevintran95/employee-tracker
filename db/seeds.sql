1a05752a5d275988022d3ff8454c9861b57cf3d8
Kevin Tran <kevin_tran95@yahoo.com>
2021-09-17 21:35:37 -0700

    added seeds

---
+++ db/seeds.sql
@@ -0,0 +1,33 @@
+INSERT INTO department (department_name)
+VALUES ("Executive Staff"),
+        ("Engineering"),
+       ("Legal"),
+       ("Finance"),
+       ('Sales');
+       
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Chief Executive Officer", 300000, 1),
+        ("Software Engineer", 150000, 2),
+        ("Head of Engineering", 200000, 2)
+        ("Lawyer", 120000, 3),
+        ("Accountant", 80000, 4),
+        ("Sales Manager", 40000, 5),
+        ("Sales Representative", 25000, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, , manager_id)
+VALUES ("Tony", "Stark", 1),
+        ("Steve", "Rodgers", 2, 1) ,
+        ("Thor", "Odinson", 3, 3),
+        ("Natasha", "Romanoff", 4, 3),
+        ("Carol", "Danvers", 5, 4),
+        ("Peter", "Parker", 6, 1)
+
+
+
+        
+
+
+
+
+
