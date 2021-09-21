5ed2752929be581c8265efb9636b55b5246e65f9
Kevin Tran <kevin_tran95@yahoo.com>
2021-09-20 21:22:08 -0700

    updated index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const express = require('express');
 const ctable = require('console.table');
+const { prompt } = require('inquirer');
 
 // Init express
 const app = express();
@@ -70,24 +71,26 @@ function homePage() {
                     updateRole();
                     break;
             }
-        })
-}
+        });
+};
 
-const viewAllDept = () => {
+const viewDepart = () => {
     const sql = "SELECT * FROM department";
     
     db.query(sql, (err, rows) => {
         console.table(res);
         homePage();
+        
     });
 }
 
-const viewAllRoles = () => {
+const viewRoles = () => {
     const sql = "SELECT * FROM role";
 
     db.query(sql, (err, rows)=> {
         console.table(res);
         homePage();
+        
     });
 }
 
@@ -97,6 +100,7 @@ const viewAllEmployees = () => {
     db.query(sql, (err, rows) => {
         console.table(res);
         homePage();
+        
     });
 }
 
@@ -113,8 +117,7 @@ const addDepartment = () => {
 
            db.query(sql, newDepart, (err, rows) => {
                console.log(`${newDepart} department was successfully added `);
-               console.log('/n');
-               init();
+               homePage();
            })
        })
     )
@@ -140,8 +143,82 @@ const addRole = () => {
         }
     ])
     .then((reply) => {
-        const sql = 
+        const sql = `INSERT INTO role (title, salary, departments_id) VALUE (?, ?, ?)`;
+        reply.departments_id = parseInt(reply.departments_id);
+        reply.newSalary = parseInt(reply.newSalary);
+        var roleArray = [reply.newRole, reply.newSalary, reply.departments_id];
+
+        db.query(sql, roleArray, (err, rows) => {
+            console.log(`${newRole} has been added to roles`);
+            homePage();
+            addRole();
+        });
+        
+    });
+}
+
+const addEmployee = () => {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            messages:'Enter the employees first name',
+            name: 'firstName'
+        },
+        {
+            type:'input',
+            message:'Enter the employees last name',
+            name: 'lastName'
+        },
+        {
+            type:'input',
+            message:'Enter the employees role ID',
+            name: 'role'
+        },
+        {
+            type:'input',
+            message:'Enter the managers id',
+            name: 'manager'
+        }
+    ])
+    .then((reply) => {
+        const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUE (?, ?, ?, ?)`;
+        reply.role = parseInt(reply.role);
+        reply.manager = parseInt(reply.manager);
+        var roleArray = [reply.firstName, reply.lastName, reply.role, reply.manager];
+
+        db.query(sql, roleArray, (err, rows) => {
+            console.log(`${reply.firstName} ${reply.lastName} has been added`);
+            
+            homePage();
+        });
+    });
+}
+
+const updateRole = () => {
+    const sql = `UPDATE employee set role_id = ? WHERE id = ?`;
+    var namesArray = [];
+
+    db.query(sql, (err,rows) => {
+        console.log(rows);
+        for (let i = 0; i < rows.length; i++);
+        let name = `${rows[i].firstName} ${rows[i].lastName}`;
+        namesArray.push(name);
     })
+    inquirer
+    .prompt([
+        {
+            type:'list',
+            message:'Select an employee to update',
+            name:'name'
+        },
+        {
+            type:'input',
+            message:'Please enter new role ID',
+            name:'update'
+        }
+    ])
+    .then((reply))
 }
 
 
@@ -149,5 +226,9 @@ const addRole = () => {
 
 
 // Listen if port is connected
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => 
+    console.log(`Server running on port ${PORT}`),
+    homePage()
+);
+
+
